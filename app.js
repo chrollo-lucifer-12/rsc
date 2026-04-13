0a4d54fad9b683b7e5222c5b472f9e259f91e72d
sahil-singh1107 <sahilpython@gmail.com>
2026-04-13 22:35:59 +0530

    add oos

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
+import { Suspense } from "react";
 import { getUsers } from "./db/data";
 
 export async function App() {
-  const users = await getUsers();
+  const usersPromise = getUsers();
 
   return (
     <div>
       <h1>list of all users</h1>
-
-      <ul>
-        {users.map((user) => {
-          return (
-            <li key={user.id}>
-              {user.name}
-              {user.email}
-            </li>
-          );
-        })}
-      </ul>
+      <Suspense fallback={<div>loading...</div>}>
+        <UsersList usersPromise={usersPromise} />
+      </Suspense>
     </div>
+  );
+}
+
+async function UsersList({ usersPromise }) {
+  const users = await usersPromise;
+  return (
+    <ul>
+      {users.map((user) => {
+        return (
+          <li key={user.id}>
+            {user.name}
+            {user.email}
+          </li>
+        );
+      })}
+    </ul>
   );
 }
--- db/data.js
+++ db/data.js
@@ -2,6 +2,14 @@ import { Database } from "bun:sqlite";
 
 const db = new Database("db/app.db");
 
+const delay = () =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 5000);
+  });
+
 export async function getUsers() {
+  await delay();
   return db.query("SELECT * FROM users").all();
 }
